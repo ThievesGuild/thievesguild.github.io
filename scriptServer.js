ed83e6128a1ee2e55e011fdd605bbee289818118
fusrohdart <nabyl0405@gmail.com>
2021-01-08 07:49:15 +0800

    Update scriptServer.js

--- scriptServer.js
+++ scriptServer.js
@@ -1,11 +1,10 @@
-require('rootpath')(); 
 const express = require('express');
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
 
-app.use('/', require("./scripts.controller"));
+app.use('https://thievesguild.github.io', require("./scripts.controller"));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on ${port}`));
