7952869578f998ad6700e5c04f8a2b7e5a3e8ed2
fusrohdart <nabyl0405@gmail.com>
2021-01-08 06:52:48 +0800

    Update getIP.js

--- Web Scripts/getIP.js
+++ Web Scripts/getIP.js
@@ -1,19 +1,9 @@
-var http = require('http');
+import { get } from 'http';
 
-http.get({ 'host': 'api.ipify.org', 'port': 80, 'path': '/' }, function (resp) {
+get({ 'host': 'api.ipify.org', 'port': 80, 'path': '/' }, function (resp) {
     resp.on('data', function (ip) {
         console.log("My public IP address is: " + ip);
         document.getElementById("IP").innerHTML = ip;
-        const fetchURL = `https://ipapi.co/${ip}/json/`;
-        fetch(fetchURL)
-            .then(function (response) {
-                response.json().then(jsonData => {
-                    document.getElementById("location").innerHTML = JSON.stringify(jsonData.ip);
-                });
-            })
-            .catch(function (error) {
-                console.log(error)
-            });
     });
 });
 
