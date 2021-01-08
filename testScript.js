80392943fa32219f1cc90638c4be9a2fe4953685
fusrohdart <nabyl0405@gmail.com>
2021-01-08 09:18:35 +0800

    Update index.html & testScript.js

--- testScript.js
+++ testScript.js
@@ -1,5 +1,7 @@
 var $ = require('jQuery');
 
-$.getJSON("http://api.ipify.org/?format=json", function(e) {
-    alert(e.ip);
-});
+$.get("http://ipinfo.io", function (response) {
+    $("#ip").html("IP: " + response.ip);
+    $("#address").html("Location: " + response.city + ", " + response.region);
+    $("#details").html(JSON.stringify(response, null, 4));
+}, "jsonp");
