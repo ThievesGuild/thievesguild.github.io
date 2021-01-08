01c7e5d960410512f2069c6134a8b34e2ebd9d68
fusrohdart <nabyl0405@gmail.com>
2021-01-08 10:07:52 +0800

    Install file-saver & Update index.html

--- testScript.js
+++
@@ -1,7 +0,0 @@
-var $ = require('jQuery');
-
-$.get("http://ipinfo.io", function (response) {
-    $("#ip").html("IP: " + response.ip);
-    $("#address").html("Location: " + response.city + ", " + response.region);
-    $("#details").html(JSON.stringify(response, null, 4));
-}, "jsonp");
