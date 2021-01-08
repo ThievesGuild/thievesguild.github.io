2314c147fac1c284ee11b77559f6772432db970f
fusrohdart <nabyl0405@gmail.com>
2021-01-08 16:46:22 +0800

    Update index.html

--- testScript.js
+++ testScript.js
@@ -3,18 +3,16 @@ const ipQuery = '/getIP?value=';
 
 const ipAddress = document.getElementById('IP').value;
 
-const getIP = () => {
-    const queryValue = ipAddress;
-    const endpoint = `${mainURL}${ipQuery}${queryValue}`;
-    
-    fetch(endpoint, {cache: 'no-cache'}).then(response => {
-      if (response.ok) {
+const queryValue = ipAddress;
+const endpoint = `${mainURL}${ipQuery}${queryValue}`;
+
+fetch(endpoint, { cache: 'no-cache' }).then(response => {
+    if (response.ok) {
         return response;
-      }
-      throw new Error('Request failed!');
-    }, networkError => {
-      console.log(networkError.message)
-    }).then((textResponse) => {
-      console.log(textResponse); 
-    })
-  }
+    }
+    throw new Error('Request failed!');
+}, networkError => {
+    console.log(networkError.message)
+}).then((textResponse) => {
+    console.log(textResponse);
+})
