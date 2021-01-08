be0f13e976a37c3f0243cb02ade7320e81407277
fusrohdart <nabyl0405@gmail.com>
2021-01-08 14:27:41 +0800

    Update scriptServer.js

--- scriptServer.js
+++ scriptServer.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 app.use(cors());
 
-app.use('/', require("./scripts.controller"));
+app.use('', require("./scripts.controller"));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on ${port}`));
